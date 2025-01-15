84fe752cbf90916e12bc4b79a1f2c31fd543ca5d
Np <152731976+corenitin@users.noreply.github.com>
2025-01-15 19:06:41 +0000

    Sum of two num

---
+++ Js-practice-quations/Practice-1/problem_02.js
@@ -0,0 +1,16 @@
+// 1st way
+// function sum(a, b) {
+//   return a + b;
+// }
+// console.log(sum(3, 4));
+
+//2nd way
+function sum(a, b) {
+    const sum = a+b
+    console.log('sum is:', sum);
+}
+sum(3123,11)
+sum(-3,-11)
+sum(-3,11)
+sum(-3,1.1)
+sum(-3.22,1.1)
